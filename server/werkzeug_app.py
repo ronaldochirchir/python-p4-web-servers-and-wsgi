b6b874b40472010fce1fd78ebf8caede5442ae6a
ronaldochirchir <ronaldochirchir8@gmail.com>
2025-03-26 00:13:17 +0300

    completed lap

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,24 @@
-#!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    # Print client information to console
+    print(f'This web server is running at {request.remote_addr}')
+    print(f'Request method: {request.method}')
+    print(f'Request path: {request.path}')
+    
+    # Create and return a response
+    return Response(
+        'A WSGI generated this response!\n'
+        f'You accessed: {request.path}\n'
+        f'Your IP: {request.remote_addr}',
+        mimetype='text/plain'
+    )
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
